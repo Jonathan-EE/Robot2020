2f7dec2126bec6621ad1737b0d62417009f1b102
Jonathan <jonathan.edwards.ee@gmail.com>
2020-02-15 19:07:49 -0600

    added test code for arduinoserial

--- src/main/java/frc/robot/subsystems/ArduinoSerial.java
+++ src/main/java/frc/robot/subsystems/ArduinoSerial.java
@@ -1,7 +1,6 @@
 
 package frc.robot.subsystems;
 
-import java.lang.String;
 import java.nio.ByteOrder;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
@@ -17,34 +16,41 @@ public class ArduinoSerial extends SubsystemBase implements Loggable{
     //SerialPort ArduinoSerial = new SerialPort(115200, ArduinoPort);
     byte[] attitude  = new byte[0];
     byte[] slice = new byte[4];
+    byte[] sliceCRC = new byte[2];
     int bytesGot;
-   SerialPort arduino;
+    int crc;
+    SerialPort arduino;
     //private final int frameHeaderByte = 0x59;
     private float angle = 999;
     private float angleTemp = 999;
-    //byte[] buffer = new byte[7];
-    //int crc;
 
     public ArduinoSerial(SerialPort serial){
         arduino = serial;
+        arduino.reset();
         arduino.setReadBufferSize(20);
-        //new Thread(this).start();
     }
 
     public float getAngle(){
         
         bytesGot = arduino.getBytesReceived();
+        bytesGot = testAttitude().length;
         if (bytesGot >= 10){
-            attitude = arduino.read(bytesGot);
+            //attitude = arduino.read(bytesGot);
+            attitude = testAttitude();
 
             for (int i=0; i<attitude.length; i++){
-                if (attitude[i] == 0x59 && attitude[i+1] == 0x59){
+                if (attitude[i] == 0x59 && attitude[i+1] == 0x42){
                     if (i+6 < attitude.length){
                         slice = Arrays.copyOfRange(attitude, i+2, i+6);
-                        angleTemp = ByteBuffer.wrap(slice).order(ByteOrder.LITTLE_ENDIAN).getFloat();
-                        if (Math.abs(angleTemp) < 360.1){
-                            angle = angleTemp;
-                        }
+                        sliceCRC = Arrays.copyOfRange(attitude,i+6,i+8);
+                        printByteArray(sliceCRC);
+                        crc = CR16.getCRC16(slice);
+                        //if (Byte.compare(crc,sliceCRC) == 0){
+                            angleTemp = ByteBuffer.wrap(slice).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+                            if (Math.abs(angleTemp) < 360.1){
+                                angle = angleTemp;
+                            }
+                        //}
                     }
                     continue;
                 }
@@ -69,5 +75,35 @@ public class ArduinoSerial extends SubsystemBase implements Loggable{
         crc = CR16.getCRC16(buffer);
         System.out.printf("%x \n", crc); 
         */
+    }
+
+    private void printByteArray(byte[] byteArray){
+        for (int i =0 ; i<byteArray.length; i++){
+            System.out.printf("%x",byteArray[i]);
+            if (i % 4 == 0){
+                System.out.printf(" ");
+            }
+        }
+        System.out.printf("\n");      
+    }
+
+    private static byte[] testAttitude(){
+        byte[] attitude  = new byte[14];
+        attitude[0] = (byte) 0x21;
+        attitude[1] = (byte) 0x59;
+        attitude[2] = (byte) 0x42;
+        attitude[3] = (byte) 0x2D;
+        attitude[4] = (byte) 0xB2;
+        attitude[5] = (byte) 0x4D;
+        attitude[6] = (byte) 0x40;
+        attitude[7] = (byte) 0x00;
+        attitude[8] = (byte) 0x00;
+        attitude[9] = (byte) 0x00;
+        attitude[10] = (byte) 0x00;
+        attitude[11] = (byte) 0x00;
+        attitude[12] = (byte) 0x00;
+        attitude[13] = (byte) 0x00;
+    
+        return attitude;
     }
 }
